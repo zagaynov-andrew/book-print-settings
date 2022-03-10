9c37655c77bda2be7e8589585c68056781700d5d
zagaynov-andrew <zagaynov.drew@gmail.com>
2022-03-10 11:47:43 +0300

    Some comments

--- book_print_settings.py
+++ book_print_settings.py
@@ -7,14 +7,13 @@ def book_print_settings(nb_pages, nb_blocks=1):
     if nb_pages == 0 or nb_blocks == 0:
         return []
     res = []
-    nb_sheets_remain = math.ceil(nb_pages / 4)
-    nb_pages_remain = nb_pages
-    nb_block_pages = 0
+    nb_sheets_remain = math.ceil(nb_pages / 4)  # number of sheets left to process
+    nb_pages_remain = nb_pages                  # number of pages  left to process
     if nb_sheets_remain < nb_blocks:
         return 'Error'
     for i in range(0, nb_blocks):
         if i == nb_blocks - 1:
-            nb_block_pages = nb_pages_remain
+            nb_block_pages = nb_pages_remain  # number of pages in a block
         else:
             nb_block_pages = math.ceil(nb_sheets_remain / (nb_blocks - i)) * 4
         start = nb_pages - nb_pages_remain
@@ -22,10 +21,11 @@ def book_print_settings(nb_pages, nb_blocks=1):
         if nb_pages_remain == 1:
             res.append((0, start + 1))
             return res
-        for j in range(0, math.ceil(nb_block_pages / 4)):
-            nb_block_max_pages = math.ceil(nb_block_pages / 4) * 4
-            left = start + nb_block_max_pages - j * 2
-            right = start + j * 2 + 1
+        nb_block_sheets = math.ceil(nb_block_pages / 4)  # number of sheets per block
+        for j in range(0, nb_block_sheets):
+            nb_block_max_pages = nb_block_sheets * 4     # maximum number of pages in a block
+            left = start + nb_block_max_pages - j * 2    # page number to print on the left
+            right = start + j * 2 + 1                    # page number to print on the right
             if left > start + nb_block_pages:
                 left = 0
             if right > start + nb_block_pages:
@@ -33,8 +33,8 @@ def book_print_settings(nb_pages, nb_blocks=1):
             res.append((left, right))
         res.append('@')
         # printing on the other side
-        for j in range(0, math.ceil(nb_block_pages / 4)):
-            nb_block_max_pages = math.ceil(nb_block_pages / 4) * 4
+        for j in range(0, nb_block_sheets):
+            nb_block_max_pages = nb_block_sheets * 4
             left = start + (j + 1) * 2
             right = start + nb_block_max_pages - 1 - j * 2
             if left > start + nb_block_pages:
